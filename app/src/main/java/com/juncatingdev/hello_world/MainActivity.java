4d7c4cf604c144b95dad4112612206f66828afe3
ELMER <rkid202001@gmail.com>
2021-03-18 21:24:10 +0800

    button

--- app/src/main/java/com/juncatingdev/hello_world/MainActivity.java
+++ app/src/main/java/com/juncatingdev/hello_world/MainActivity.java
@@ -3,6 +3,9 @@ package com.juncatingdev.hello_world;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button customButton = findViewById(R.id.custom_button);
+
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
